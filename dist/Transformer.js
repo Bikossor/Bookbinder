61794f462c5bed872237b9e48adcf7af390cb8c9
Bikossor <andre.lichtenthaeler@gmail.com>
2019-10-27 21:41:37 +0100

    Closes #4

---
+++ dist/Transformer.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Transformer {
+    static transform(files) {
+    }
+}
+exports.Transformer = Transformer;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiVHJhbnNmb3JtZXIuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvVHJhbnNmb3JtZXIudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFFQSxNQUFhLFdBQVc7SUFDYixNQUFNLENBQUMsU0FBUyxDQUFDLEtBQWM7SUFFdEMsQ0FBQztDQUNKO0FBSkQsa0NBSUMifQ==
---
+++ src/Transformer.ts
@@ -0,0 +1,7 @@
+import { IFile } from "./Interfaces/IFile";
+
+export class Transformer {
+    public static transform(files: IFile[]) {
+
+    }
+}
