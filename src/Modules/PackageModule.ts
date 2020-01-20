ec133beec5f6b094e10238867a0aad1e8986693a
Bikossor <andre.lichtenthaeler@gmail.com>
2020-01-21 00:16:11 +0100

    Closes #28

--- src/Modules/PackageModule.ts
+++ src/Modules/PackageModule.ts
@@ -1,5 +1,16 @@
+import * as path from "path";
+
 export class PackageModule {
     public static run(args?: Array<string>): string {
-        return args.join("-");
+        const currentDirectory: string = process.cwd();
+        const packagePath: string = path.join(currentDirectory, "package.json");
+        const currentPackage: Object = require(packagePath);
+        const packageProperty: string = args[0];
+
+        if (!packageProperty && !currentPackage.hasOwnProperty(packageProperty)) {
+            throw new Error(`Property "${packageProperty}" of "${packagePath}" was not found!`);
+        }
+
+        return currentPackage[packageProperty];
     }
 }
